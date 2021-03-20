7f31fb15c84eb8e35b2ea8e61abec55fcf09d404
Mustafa <mustafa.alotbah@gmail.com>
2021-03-20 20:04:14 +0100

    finished off the rasterizer

--- Archive/EmptyRasterizer/rasterizer.cpp
+++ Archive/EmptyRasterizer/rasterizer.cpp
@@ -1,44 +1,65 @@
 
 
-#include"main.h"
+#include"rasterizer.h"
 
 
 
 namespace mge
 {
+	/*  (De)Constructing the rasterizer  */
 	Rasterizer::Rasterizer(VideoBuffer buffer) {
 		this->buffer = buffer;
 	}
-	
+
 	Rasterizer::~Rasterizer() {
 
 	}
+
+
+
+
+
+
+
+
+	/*  The work of the rasterizer  */
 
 	bool Rasterizer::OnLoad() {
 		return true;
 	}
 
 
-
-
-
-
-
-
-
-
-
+	float time = 0;
 
 	bool Rasterizer::OnUpdate(float deltaTime) {
+
+
+		/* dummy animation */
+		time = time + deltaTime;
+		if (time > 5)
+		{
+			time = 0;
+		}
 		for (int y = 0; y < this->buffer.height; y++)
 		{
 			for (int x = 0; x < this->buffer.width; x++)
 			{
-				*((unsigned int*)this->buffer.addr + y * this->buffer.width + x) = x;
+				*((unsigned int*)this->buffer.addr + y * this->buffer.width + x) = x * time;
 			}
 		}
+
+
+
+
+
+
+		/* successfully computed */
 		return true;
 	}
+
+
+
+
 
 
 }
--- rasterizer.cpp
+++ rasterizer.cpp
@@ -1,11 +1,12 @@
 
 
-#include"main.h"
+#include"rasterizer.h"
 
 
 
 namespace mge
 {
+	/*  (De)Constructing the rasterizer  */
 	Rasterizer::Rasterizer(VideoBuffer buffer) {
 		this->buffer = buffer;
 	}
@@ -14,31 +15,51 @@ namespace mge
 
 	}
 
+
+
+
+
+
+
+
+	/*  The work of the rasterizer  */
+
 	bool Rasterizer::OnLoad() {
 		return true;
 	}
 
 
-
-
-
-
-
-
-
-
-
+	float time = 0;
 
 	bool Rasterizer::OnUpdate(float deltaTime) {
+
+
+		/* dummy animation */
+		time = time + deltaTime;
+		if (time > 5)
+		{
+			time = 0;
+		}
 		for (int y = 0; y < this->buffer.height; y++)
 		{
 			for (int x = 0; x < this->buffer.width; x++)
 			{
-				*((unsigned int*)this->buffer.addr + y * this->buffer.width + x) = x;
+				*((unsigned int*)this->buffer.addr + y * this->buffer.width + x) = x * time;
 			}
 		}
+
+
+
+
+
+
+		/* successfully computed */
 		return true;
 	}
+
+
+
+
 
 
 }
